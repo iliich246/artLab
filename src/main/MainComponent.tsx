e24b3c91e2e1642949b60144b51b36736ed1e95a
iliich246 <iliich246@gmail.com>
2023-12-15 02:35:00 +0400

    Disable strict mode

--- src/main/MainComponent.tsx
+++ src/main/MainComponent.tsx
@@ -32,7 +32,7 @@ function MainComponent() {
     <Box sx={{
       width: '100%',
       height: '100%',
-      backgroundColor: "#ccc"
+      backgroundColor: "transparent",
     }}>
       <Typography sx={{
         color: "#fff",
@@ -45,7 +45,8 @@ function MainComponent() {
         sx={{
           position: "absolute",
           top: 0,
-          height: "100vh"
+          height: "100vh",
+          zIndex: -1
         }}
       />      
     </Box>
